96de886f4a5d3ee22706fcedd4d656729aa8e05c
John Hopkins <johnthopkins@gmail.com>
2016-08-09 11:06:58 -0700

    started adding classes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const rl = require('readline-sync')
+const interface = require('./lib/interface')
 const add = require('./lib/add')
 
-var firstnumber = parseInt(rl.question('whats the first number you want to add?'), 10)
-var secondnumber = parseInt(rl.question('whats the second number you want to add?'), 10)
+// Introduce Blackjack
+console.log('Welcome Pocket Aces BlackJack! Created by Ana, Jared and John')
+console.log('Basic rules: Play against the dealer with the goal of getting a hand with a value of 21')
+console.log('Face cards are worth 10pts other cards are worth their face value. Aces are either 1 or 11')
+console.log('Dealer deals 1 card face up and one face down to each player starting at his left to begin')
 
-console.log(add(firstnumber, secondnumber))
-
+// var players = interface.askForNumber('How many human players do you want to create?')
+// console.log(players)
+// console.log(typeof players)
