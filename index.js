532fd70db04f955e39aea3d13cc0eb7f426c06a2
John Hopkins <johnthopkins@gmail.com>
2016-08-09 15:14:54 -0700

    added human and AI players to came with names

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const Deck = require('./lib/deck')
 const game = require('./lib/game')
-
+const Player = require('./lib/player')
 const interface = require('./lib/interface')
 const add = require('./lib/add')
 
@@ -9,9 +10,34 @@ console.log('Basic rules: Play against the dealer with the goal of getting a han
 console.log('Face cards are worth 10pts other cards are worth their face value. Aces are either 1 or 11')
 console.log('Dealer deals 1 card face up and one face down to each player starting at his left to begin')
 
+var gameOptions = {
+  humans: [],
+  ai: [],
+
+}
+
 var players = interface.askForNumber('How many human players do you want to create?')
- console.log("You have:" + " " players + " " + "players in this round")
-// console.log(typeof players)
+ console.log('You have:'+' '+players+' human players in this round')
+ for (i = 0; i < players; i++){
+  var player = new Player;
+  var name = interface.ask('What is this player\'s name?')
+  player.type = 'human'
+  player.name = name
+  gameOptions.humans.push(player)}
+
+  var ai = interface.askForNumber('How many computer players do you want to create?')
+  console.log('You have:'+' '+players+' computer players in this round')
+   for (i = 0; i < ai; i++){ 
+    var player = new Player
+    player.type = 'AI'
+    player.name = ('AI '+ (i+1))
+    gameOptions.ai.push(player)
+  }
 
 var ourGame = new game
-console.log(ourGame)
+ourGame.players.push(gameOptions)
+ourGame.decks.push(new Deck)
+
+
+  console.log(ourGame)
+  console.log(ourGame.decks)
